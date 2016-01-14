5603ecad97f495c80b9c6476a457567ee8adf81d
David Ruiz Vallejo <davizjo@gmail.com>
2016-01-14 18:40:50 +0100

    linker project properties

--- Golden Axe/Golden Axe/Main.cpp
+++ Golden Axe/Golden Axe/Main.cpp
@@ -4,8 +4,8 @@
 
 #include "SDL/include/SDL.h"
 
-//#pragma comment( lib, "SDL/libx86/SDL2.lib" )
-//#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 
 enum main_states
 {
